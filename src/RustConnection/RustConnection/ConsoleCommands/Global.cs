ff8f4bc75c2c684f454e631b049b2cb4b503975c
TheRyuzaki <lod.skot@gmail.com>
2020-03-28 11:14:57 +0400

    [ConsoleCommands\Global] Relaes command "help'

--- src/RustConnection/RustConnection/ConsoleCommands/Global.cs
+++ src/RustConnection/RustConnection/ConsoleCommands/Global.cs
@@ -1,4 +1,6 @@
-﻿using RustConnection.Manager;
+﻿using System;
+using System.Reflection;
+using RustConnection.Manager;
 
 namespace RustConnection.ConsoleCommands
 {
@@ -8,6 +10,35 @@ namespace RustConnection.ConsoleCommands
         static void ExitCommand(string command, string[] args)
         {
             AppManager.Instance.IsWork = false;
+        }
+
+        [ConsoleManager.ConsoleCommandAttribute("help")]
+        static void HelpCommand(string command, string[] args)
+        {
+            Console.WriteLine("### List commands:");
+            
+            Type[] types = typeof(ConsoleManager).Assembly.GetTypes();
+            for (var i = 0; i < types.Length && AppManager.Instance.IsWork; i++)
+            {
+                MethodInfo[] methods = types[i].GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.CreateInstance | BindingFlags.Static);
+                for (var j = 0; j < methods.Length && AppManager.Instance.IsWork; j++)
+                {
+                    object[] attrs = methods[j].GetCustomAttributes(typeof(ConsoleManager.ConsoleCommandAttribute), false);
+                    if (attrs.Length > 0)
+                    {
+                        ConsoleManager.ConsoleCommandAttribute attr = (ConsoleManager.ConsoleCommandAttribute) attrs[0];
+                        Console.WriteLine("# Command: " + attr.Command);
+                        if (string.IsNullOrEmpty(attr.Syntax) == false)
+                        {
+                            Console.WriteLine("- Sytax: " + attr.Syntax);
+                        }
+                        if (string.IsNullOrEmpty(attr.Description) == false)
+                        {
+                            Console.WriteLine("- Description: " + attr.Description);
+                        }
+                    }
+                }
+            }
         }
     }
 }
